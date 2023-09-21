136a10ddefe3102aa5efc2b89e9d435434905f8e
Wirooo03 <145635758+Wirooo03@users.noreply.github.com>
2023-09-21 11:21:35 +0700

    Update and rename contph.c to contoh.c

---
+++ contoh.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <>
+
+int main (){
+  int X;
+  int Y;
+  printf("ketik angka pertama anda:");
+  scanf("%d", &X);
+  printf("ketik angka kedua anda:");
+  scanf("%d", &Y);
+  int Z = X + Y;
+  printf("jumlah kedua angka tersebut adalah: %d", Z);
+}
--- contph.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main (){
-  printf("Hello WOrdl!");
-  return 0;
-}
